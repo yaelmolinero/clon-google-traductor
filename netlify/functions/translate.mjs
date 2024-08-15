669202859f5bb52cb89e6f7ed309da641e76a030
Yael Molinero <y.molinero20@gmail.com>
2024-08-15 12:10:45 -0600

    Add errors feedback

--- netlify/functions/translate.mjs
+++ netlify/functions/translate.mjs
@@ -12,7 +12,11 @@ export default async function (req)  {
     const response = await translator.translateText(text, handlFromLanguage, toLanguage);
     const result = JSON.stringify(response.text);
     return new Response(result);
-  } catch(err) {
-    return new Response("Error");
+  } catch {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "October", "November", "December"];
+    const date = new Date();
+    date.setMonth(date.getMonth() + 1);
+    const errorMessage = `Free api use terminated this month, try again on ${months[date.getMonth()]} 12 👋`;
+    return new Response(JSON.stringify(errorMessage));
   }
 }
