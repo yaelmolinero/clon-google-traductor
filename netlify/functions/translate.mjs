6d3189883f888d449d6a2000c00e910f88e90dfb
Yael Molinero <y.molinero20@gmail.com>
2024-08-15 11:33:52 -0600

    Add serverless function

--- netlify/functions/translate.mjs
+++ netlify/functions/translate.mjs
@@ -1,25 +1,18 @@
-const URL = "https://api-free.deepl.com/v2/translate";
+import * as deepl from "deepl-node";
+
 const apiKey = process.env.API_KEY;
-console.log(apiKey);
+const translator = new deepl.Translator(apiKey);
 
-export default async function (req) {
-  const { text, fromLanguage, toLanguage } = JSON.parse(req.body);
+export default async function (req)  {
+  const data = await req.json();
+  const { text, fromLanguage, toLanguage } = data;
+  const handlFromLanguage = fromLanguage === "auto" ? null : fromLanguage;
 
-  const response = await fetch(URL, {
-    method: "POST",
-    headers: {
-      Authorization: `DeepL-Auth-Key ${apiKey}`,
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      Connection: "keep-alive"
-    },
-    body: JSON.stringify({
-      text: [text],
-      "target_lang": toLanguage,
-      "source_lang": fromLanguage
-    })
-  });
-  const result = await response.json();
-
-  return new Response(result.traslations[0].text);
+  try {
+    const response = await translator.translateText(text, handlFromLanguage, toLanguage);
+    const result = JSON.stringify(response.text);
+    return new Response(result);
+  } catch(err) {
+    return new Response("Error");
+  }
 }
